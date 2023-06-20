2e4d0726a3936cda419dd8d9ec6ff9c169eab42d
22 <87944616+tuvvshe@users.noreply.github.com>
2023-06-20 20:54:19 +0200

    Add files via upload

---
+++ scatter_plot.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+rng = np.random.RandomState(0)
+
+x = rng.randn(100)
+
+y = rng.randn(100)
+
+colors = rng.rand(100)
+
+sizes = 1000 * rng.rand(100)
+
+
+plt.scatter(x, y, c=colors, s=sizes, alpha=0.3,
+            cmap='virids')
+plt.colorbar()
+plt.show()
